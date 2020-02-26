f28cbefe755a93fe0115b4f1a7da072ffbaf6c4e
Peter(Yongsik) Moon <peter.moon@lumicom.com.au>
2020-02-26 11:45:37 +1100

    move babel config to webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -26,7 +26,19 @@ module.exports = {
                     use: {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env', '@babel/preset-react']
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        "useBuiltIns": "usage",
+                                        "corejs": { version: 3, proposals: true },
+                                        "debug": true,
+                                        //"modules": false,
+                                        //"loose": true
+                                    }
+                                ],
+                                ['@babel/preset-react']
+                            ]
                         }
                     }
                 },
